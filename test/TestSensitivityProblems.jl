e3db8de2b83ad92fc9a0118a8004f80ec272d2dd
Damir Akchurin <AkchurinDA@gmail.com>
2024-05-07 21:04:49 -0400

    Update random variable

--- test/TestSensitivityProblems.jl
+++ test/TestSensitivityProblems.jl
@@ -74,6 +74,49 @@ end
 end
 
 @testset "Sensitivity Problems (Type II) #2" begin
+    # Example 6.7 (p. 161) from "Structural and System Reliability" book by Armen Der Kiureghian
+
+    # Define the random vector as a function of its moments:
+    function XFunction(Θ::Vector)
+        M₁ = randomvariable("Normal",  "M", [Θ[1], Θ[2]])
+        M₂ = randomvariable("Normal",  "M", [Θ[3], Θ[4]])
+        P  = randomvariable("Gumbel",  "M", [Θ[5], Θ[6]])
+        Y  = randomvariable("Weibull", "M", [Θ[7], Θ[8]])
+
+        return [M₁, M₂, P, Y]
+    end
+
+    # Define the correlation matrix:
+    ρˣ = [
+        1.0 0.5 0.3 0.0
+        0.5 1.0 0.3 0.0
+        0.3 0.3 1.0 0.0
+        0.0 0.0 0.0 1.0]
+
+    # Define the parameters of the random vector:
+    Θ = [
+        250,   250   * 0.30,
+        125,   125   * 0.30,
+        2500,  2500  * 0.20,
+        40000, 40000 * 0.10]
+
+    # Define the limit state function:
+    a            = 0.190
+    s₁           = 0.030
+    s₂           = 0.015
+    g(x::Vector) = 1 - x[1] / (s₁ * x[4]) - x[2] / (s₂ * x[4]) - (x[3] / (a * x[4])) ^ 2
+
+    # Define a sensitivity problem:
+    Problem  = SensitivityProblemTypeII(XFunction, ρˣ, g, Θ)
+
+    # Perform the sensitivity analysis:
+    Solution = solve(Problem)
+
+    # Test the results:
+    @test isapprox(Solution.∇β, 1E-3 * [-3.240, -3.920, -6.480, -7.840, -0.546, -0.790, 0.124, -0.245], rtol = 0.05)
+end
+
+@testset "Sensitivity Problems (Type II) #3" begin
     # "FORM Sensitivities to Distribution Parameters with the Nataf Transformation"
     # by J.-M. Bourinet
     # 10.1007/978-3-319-52425-2_12
@@ -112,7 +155,7 @@ end
     @test isapprox(Solution.∇β, [-0.0059, -0.0079, -0.0009, -0.0006, 1.2602, -1.1942], rtol = 0.01)
 end
 
-@testset "Sensitivity Problems (Type II) #3" begin
+@testset "Sensitivity Problems (Type II) #4" begin
     # "FORM Sensitivities to Distribution Parameters with the Nataf Transformation"
     # by J.-M. Bourinet
     # 10.1007/978-3-319-52425-2_12
